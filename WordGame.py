92c2c4cf0eb05c8e860cb7d867cc8a240080bdc9
Reeves <you@example.com>
2024-03-15 01:03:36 -0700

    Edited some wording and added more line spacing and longer pauses between lines

--- WordGame.py
+++ WordGame.py
@@ -1,18 +1,20 @@
 import time
 PlayerName = input('Hello! Welcome to my word guessing game!  What is your name?   ')
 print ('')
-print ('The objective of this game is to try to guess a chosen word in 11 guesses or less')
+print ('The objective of this game is to try to correctly guess a hidden word')
+time.sleep(2)
 print ('Good luck, ' + PlayerName + '!')
+time.sleep(2)
 
 print('')
 
 
 def start():
     print(' ')
-    print('I have thought of a word')
+    print('I have thought of a word. Each line below represents a letter in it.')
     SecretWord = 'ostrich'
     Guesses = ''
-    RemainingTurns = 11
+    RemainingTurns = 10
     while RemainingTurns > 0:
         Wrong_Answers = 0
         for Letter in SecretWord:
@@ -28,31 +30,37 @@ def start():
             print('')
             break
         print('')
-        Guess = input('Guess a letter that might be in the word:   ').lower()
+        Guess = input('Guess a letter that might be in the hidden word above:   ').lower()
         Guesses += Guess
+        print('')
 
         if Guess not in SecretWord:
             RemainingTurns -= 1
             print('')
-            print('Sorry. That letter is not in the word.')
-            print('You have ' + str(RemainingTurns) + ' more guess left.')
-            print('Please try again')
+            print('💥 🙅 💥')
+            print('')
+            print('Sorry. The letter ' + Guess.capitalize() + ' is not in the word.')
+            print('You are allowed ' + str(RemainingTurns) + ' more incorrect guesses.')
+            print(' ')
             if RemainingTurns == 0:
-                print('I\'m sorry, ' + PlayerName + ', but you have run out of available guesses. ☹ You lost.')
+                print('I\'m sorry, ' + PlayerName + ', but you have run out of available guesses. ☹  You lost.')
+                print('')
+                time.sleep(.5)
                 print('The word was: ' + SecretWord )
-                print('GAME OVER. Better luck next time!')
+                print('')
+                print('☠️  Better luck next time! ☠️')
                 time.sleep(1)
     def Restart():
         print('')
         NewGame = input('Yes or no, would you like to play again?  ').lower()
         print('')
         if NewGame == 'no':
-            print('Thank you for having played!  Good-bye, ' + PlayerName +'!')
+            print('Thank you for having played! 👋 Goodbye, ' + PlayerName +'!')
             time.sleep(1.75)
             quit()
         if NewGame == 'yes':
-            print('Wonderful!  I will come up with a new word for you to guess!')
-            time.sleep(2)
+            print('Wonderful! 😄 I will come up with a new word for you to guess!')
+            time.sleep(3)
             start()
         else:
             print('')
